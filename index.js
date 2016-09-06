0d80525175dd361ba0e7186d44366f08b2a815b8
Joshua McClure <josh@joshuadmcclure.com>
2016-09-06 14:57:10 -0400

    Added an extra condition to check for a successful response code. This module was causing the entire app to fail if the Logi app server is not on. Moved the module requests out of the function.

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
+var request = require("request"),
+	querystring = require('querystring')
+		
 exports.createToken = (urlObj,userObj,paramsObj,callback)=>{
-	var request = require("request"),
-		querystring = require('querystring'),
-		validUrlObj = (urlObj && typeof urlObj !== 'undefined' && typeof urlObj === 'object')? true: false,
-		url = {
-			protocol: (validUrlObj && urlObj.hasOwnProperty('protocol'))? urlObj.protocol: 'http',
-			hostname: (validUrlObj && urlObj.hasOwnProperty('hostname'))? urlObj.hostname: 'localhost',
-			pathname: (validUrlObj && urlObj.hasOwnProperty('pathname'))? urlObj.pathname: '',
-			port: (validUrlObj && urlObj.hasOwnProperty('port'))? ':'+urlObj.port: ''
-		};
+		var validUrlObj = (urlObj && typeof urlObj !== 'undefined' && typeof urlObj === 'object')? true: false,
+			url = {
+				protocol: (validUrlObj && urlObj.hasOwnProperty('protocol'))? urlObj.protocol: 'http',
+				hostname: (validUrlObj && urlObj.hasOwnProperty('hostname'))? urlObj.hostname: 'localhost',
+				pathname: (validUrlObj && urlObj.hasOwnProperty('pathname'))? urlObj.pathname: '',
+				port: (validUrlObj && urlObj.hasOwnProperty('port'))? ':'+urlObj.port: ''
+			};
 		
 	try{		
 		// Add a forward slash to the begining of the pathname property and remove any trailing slashes.
@@ -23,7 +24,7 @@ exports.createToken = (urlObj,userObj,paramsObj,callback)=>{
 				if(!error && response.statusCode === 200){
 					callback(null,body);
 				}else{
-					if(response.statusCode === 404){
+					if(response && response.statusCode === 404){
 						callback('Error 404: It seems that the URL to your Logi application is not correct. Please correct the URL and try again.');					
 					}else{
 						callback(error);
